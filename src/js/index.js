f0238fc1307bde7f3ecdb2e0a8089627e58823c4
AshforthM <ashforth.m@gmail.com>
2022-02-04 21:36:50 -0700

    completed basic css/html layout

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,14 @@
 import { ref as dataRef, get, set, update } from "firebase/database";
 import { db } from "./libs/firebase/firebaseConfig";
 
-
 console.log(db);
+
+function init(){
+
+}
+
+//DELETE
+
+function deleteItem(){
+
+}
