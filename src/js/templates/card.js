e71ef8c28926ad6f3b7787cddd4e4285f8a3735e
AshforthM <ashforth.m@gmail.com>
2022-02-12 17:18:44 -0700

    basic styling applied
    colours, logos, etc. completed

--- src/js/templates/card.js
+++ src/js/templates/card.js
@@ -3,16 +3,18 @@ import { db } from "../libs/firebase/firebaseConfig";
 
 export default function renderCard({urlPath, key, productName, price, blurb}) {
   const template = `
-    <li class="card">
-    <img src="${urlPath}" />
+  <li class="card">
+    <div class="img-container">
+      <img src="${urlPath}" />
+    </div>
     <ul class="card-options">
-      <li><i class="fa-solid fa-pencil" id="update" data-key="${key}"></i></li>
+      <li><i id="update" data-key="${key}" class="fa-solid fa-pencil"></i></li>
       <li><i id="delete" data-key="${key}" class="fa-solid fa-trash"></i></li>
     </ul>
     <div class="product-details">
       <span class="product-name">${productName}</span>
       <span class="price">${price}</span>
-      <span class="reviews">${blurb}</span>
+      <span class="blurb">${blurb}</span>
       <button class="btn-buy">Buy Now</button>          
     </div>            
   </li>
